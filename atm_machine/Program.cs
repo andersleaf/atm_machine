8bee54e922c8dc30d445710586260c5e56f59f32
andersleaf <andersleaf@gmail.com>
2025-02-13 11:44:38 -0800

    Completed!

--- atm_machine/Program.cs
+++ atm_machine/Program.cs
@@ -13,25 +13,46 @@ namespace atm_machine
 
         public void Options()
         {
+            Console.WriteLine("");
             Console.WriteLine($"What would you like to do today {username}? ");
             Console.WriteLine("1. Deposit money");
             Console.WriteLine("2. Withdraw money");
             Console.WriteLine("3. Check your balance");
+            Console.WriteLine("");
             string choice = Console.ReadLine();
             if (choice == "1")
             {
-                Console.Write("Please enter the amount of money you wish to deposit: ");
+                Console.WriteLine("");
+                Console.WriteLine("Please enter the amount of money you wish to deposit: ");
+                Console.WriteLine("");
                 int amount = Convert.ToInt32(Console.ReadLine());
                 Deposit(amount);
+                Options();
             }
-        }
-        public int Balance(int total, int amount)
-        {
-            return total; // Stores the balance of the bank account
+            else if (choice == "2")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter the amount of money you with to withdraw: ");
+                Console.WriteLine("");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                Withdraw(amount);
+                Options();
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("");
+                Get_Balance(total);
+                Options();
+            }
+            else
+            {
+                Console.WriteLine("Invalid command. Please choose from the following options: ");
+                Options();
+            }
         }
         public void Get_Balance(int total)
         {
-            Console.WriteLine(total); //Displays the balance of the bank account to the user
+            Console.WriteLine("Your balance is: $" + total);
         }
         public void Create()
         {
@@ -46,15 +67,17 @@ namespace atm_machine
         public int Deposit(int amount)
         {
             total = amount + total;
-            Console.WriteLine("Your new total is " + total);
+            Console.WriteLine("");
+            Console.WriteLine("Your new total is: $" + total);
             return total;
 
         }
         public int Withdraw(int amount)
         {
-            Console.Write("Please enter the amount of money you with to withdraw: ");
-            string a = Console.ReadLine();
-            return Convert.ToInt32(a);
+            total = total - amount;
+            Console.WriteLine("");
+            Console.WriteLine("Your new total is: $" + total);
+            return total;
         }
     }
     class Program
